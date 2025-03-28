fab58762cf5850c495a62a3e8590a9acefa5855e
Mohit-07-js <mohitnemade1977@gmail.com>
2025-03-28 11:55:09 +0000

    comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,33 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+
+
+// console.log("2"> 1);
+// console.log("02"> 1);// it prints true as an output
+// //javascript will automatically your string into number
+
+
+console.log(null > 0);
+
+console.log(null == 0);
+
+console.log(null >= 0);
+// null is an empty value  therefore it is not geater than 0 and in case null value converted to 0 is> = 0 then it prints true
+
+console.log(undefined == 0 );
+console.log(undefined > 0 );
+console.log(undefined < 0 );
+// in case of undefined it prints 0 which is false
+//  in java script comparision and equality check both functions work differnt
+// also syantx of == and > = are aslo differnt from each other 
+// and < andf > aslo as there own syantax
+// ===    it not only check the value strictly but it check aslo data types
+console.log("2" === 2);
+//it give output false because the both are differnt data type
+
+ 
