426a34e6d527f31faf4b6cb716cbca15e3fbfeb2
Mohit-07-js <mohitnemade1977@gmail.com>
2025-03-28 09:50:37 +0000

    conversion of data types

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+//if sore is null then ,object,number, 0 will be print
+//if score is undefined then undefined,undefined,number ,nan will be print
+// if boolen  value true is there then boolean,boolean,number,1 will be printed
+// if only string like "mohit" then string,string,number,nan will be printed
+// "33" =>33
+// "33abc" => nan (not a number)
+// true => 1 , false => 0
+let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// isloggedin-  "" => false
+// -- "mohit"  => true
+// o => false
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof(stringNumber));
+
+console.log(stringNumber);
