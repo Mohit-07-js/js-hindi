190e398b65559bdfd2eaff058cf8780fafb0f6dd
Mohit-07-js <mohitnemade1977@gmail.com>
2025-03-28 10:25:24 +0000

    conversion part2

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -19,7 +19,55 @@ let isLoggedIn = 1
 // -- "mohit"  => true
 // o => false
 let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(typeof(stringNumber));
+// let stringNumber = String(someNumber)
+// console.log(typeof(stringNumber));
 
-console.log(stringNumber);
+// console.log(stringNumber);
+
+
+
+/// ****************************operations*******************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " mohit"
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2+"3");
+
+
+// console.log((3 +4)* 5 %3);
+// console.log(+true);// if + is added to first but true it self is a bollean there fore it cannot increment therefore it prints true
+
+// console.log(+"");//prints 0 
+
+
+let num1,num2 ,num3
+
+num1 = num2 = num3 =2+2
+
+
+let gameCounter = 100
+++gameCounter;// prefix and postfix
+// console.log(gameCounter);
+
+
+
+
+
+
