1ef86f72eaf37f6ff08829c2d29e322f9af98460
Mohit Nemade <mohitnemade1977@gmail.com>
2025-09-13 09:45:25 +0000

    conditional if

---
+++ 04_control_flow/one.js
@@ -0,0 +1,34 @@
+//if
+const userIloggedIn = true
+const number =41
+if (userIloggedIn){
+    console.log("executed");
+    
+}
+if (number<=50){
+    console.log("executed");
+    
+}
+else{
+    console.log("not executed");
+    
+}
+const score = 200
+if (score>100){
+    const power = "fly"//let
+    console.log(`user power ${power}`);
+    
+}
+
+const userIloggedin = true
+const DebtCard = true
+const loggedinformGoogle = false
+const loggedinfromEmail = true
+if(userIloggedIn && DebtCard){
+    console.log("allow to buy course ");
+    
+}
+if(loggedinformGoogle || loggedinfromEmail){
+    console.log("user logged in");
+    
+}
