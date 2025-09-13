0feb1466411f169a8ba1ae6f1efa60afcb61fc5f
Mohit Nemade <mohitnemade1977@gmail.com>
2025-09-13 07:13:58 +0000

    scops

--- 03_basics/02_scops.js
+++ 03_basics/02_scops.js
@@ -3,13 +3,38 @@ if (true){
     let a= 10 
     const b = 20 
     // var c = 30// 
-    console.log("inner :",a);
+    //console.log("inner :",a);
     
 
 }
 
-console.log(a);
+//console.log(a);
 // console.log(b);
 // console.log(c);
 
+function one(){
+    const username = "Mohit"
+    function two(){
+        const Website = "youtube"
+       // console.log(username);
 
+        
+    }
+    two()
+    //console.log(Website);
+
+    
+}
+one()
+
+function one(num){
+    return num +1
+}
+console.log(one(5));
+
+
+/// the add two store h=the value in the variable that cause error is it initialize before the  =function 
+const Addtwo = function(num){
+    return num +1
+}
+Addtwo(5)
