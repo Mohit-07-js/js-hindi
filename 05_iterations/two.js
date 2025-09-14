6ee3ce765edeeb88180822ec94cef5ae8c99e359
Mohit Nemade <mohitnemade1977@gmail.com>
2025-09-14 04:50:16 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+// let val1 = 0
+// while(val1<10){
+//     console.log(`the vallue of i is: ${val1}`);
+//     val1 = val1+2
+    
+// }
+let myarray = ["baatman", "superman", "spiderman", "hulk"]
+let index = 0
+while(index < myarray.length){
+    console.log(`the value is: ${myarray[index]}`);
+    index = index +1
+}
+let score = 11
+do {
+    console.log(`the score is ${score}`);
+    score++
+    
+    
+} while (score<10);
